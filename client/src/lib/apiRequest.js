2c0a1dc38d4d873c4f77feda4f0d4abf8c793ae9
SukarayamJ <sukarayam@mfleduclub.com>
2024-07-13 03:47:28 +0530

    added backend url

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://urban-nest-deploy.onrender.com/api",
   withCredentials: true,
 });
 
