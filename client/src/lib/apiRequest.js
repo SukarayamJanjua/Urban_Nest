83e26da8621a39ebd8c44907271d1af8f2fe66b7
Sukarayam Janjua <94704551+SukarayamJanjua@users.noreply.github.com>
2024-07-13 02:41:12 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://urban-nest-api-delta.vercel.app/api",
   withCredentials: true,
 });
 
